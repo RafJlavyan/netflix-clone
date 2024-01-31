94de994e4ba240fe0144b8261a0bf07f1a285b14
RafJlavyan <jlavyan2021@gmail.com>
2024-01-31 14:33:33 +0400

    Some changes

--- src/utils/firebase.config.js
+++ src/utils/firebase.config.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9l6ql6extKjSdWHx4vNd1y2_8ioFBxwo",
-  authDomain: "learning-projects-84791.firebaseapp.com",
-  projectId: "learning-projects-84791",
-  storageBucket: "learning-projects-84791.appspot.com",
-  messagingSenderId: "400317284636",
-  appId: "1:400317284636:web:0081ee0bcf93c74ab3bad9",
-  measurementId: "G-DJH06M7Q5C",
+ // firebase config
 };
 
 const app = initializeApp(firebaseConfig);
